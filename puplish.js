d2b58d4a24a4a88c8d4be7d365fd29a547a396cb
Tasneem maqableh <103468147+engTasneemmaq@users.noreply.github.com>
2022-07-13 17:30:40 +0300

    Update puplish.js

--- puplish.js
+++ puplish.js
@@ -3,15 +3,15 @@ const AWS = require('aws-sdk');
 require('dotenv').config();
 AWS.config.update({
     region: 'us-east-1',
-    accessKeyId: process.env.ACCESS,
-    secretAccessKey: process.env.SECRET
+    accessKeyId: process.env.AWS_ACCESS_ID,
+    secretAccessKey: process.env.AWS_SECRET_KEY
 });
 
 function sendMessage() {
     const messageParams = {
         Message: 'use this code to save 70% of your next order : 70free',
         // TopicArn: process.env.TOPIC,
-        TopicArn: 'arn:aws:sns:us-east-1:942186265041:GIT-TOP'
+        TopicArn: 'arn:aws:sns:us-east-1:919957578830:cool-notification'
     }
     const publishMsg = new AWS.SNS({
         apiVersion: '2010-03-31'
